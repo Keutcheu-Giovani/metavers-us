4d28fed631bacf26e59f159a6e25e17d9b1fd397
Keutcheu Giovani <keutcheugiovaniy@gmail.com>
2023-12-30 22:48:48 +0100

    update

---
+++ src/assets/index.js
@@ -0,0 +1,58 @@
+import arrowDown from "./arrow-down.svg";
+import arrow from "./arrow.svg";
+import facebook from "./facebook.svg";
+import headset from "./headset.svg";
+import instagram from "./instagram.svg";
+import linkedin from "./linkedin.svg";
+import menu from "./menu.svg";
+import search from "./search.svg";
+import twitter from "./twitter.svg";
+import vercel from "./vercel.svg";
+import vrpano from "./vrpano.svg";
+import cover from "./cover.png";
+import getStarted from "./get-started.png";
+import map from "./map.png";
+import people1 from "./people-01.png";
+import people2 from "./people-02.png";
+import people3 from "./people-03.png";
+import planet1 from "./planet-01.png";
+import planet2 from "./planet-02.png";
+import planet3 from "./planet-03.png";
+import planet4 from "./planet-04.png";
+import planet5 from "./planet-05.png";
+import planet6 from "./planet-06.png";
+import planet7 from "./planet-07.png";
+import planet8 from "./planet-08.png";
+import planet9 from "./planet-09.png";
+import stamp from "./stamp.png";
+import whatsNew from "./whats-new.png";
+export {
+    people1,
+    people2,
+    people3,
+    planet1,
+    planet2,
+    planet3,
+    planet4,
+    planet5,
+    planet6,
+    planet7,
+    planet8,
+    planet9,
+    search,
+    stamp,
+    whatsNew,
+    map,
+    menu,
+    getStarted,
+    cover,
+    arrow,
+    arrowDown,
+    facebook,
+    headset,
+    instagram,
+    linkedin,
+    twitter,
+    vercel,
+    vrpano,
+};
